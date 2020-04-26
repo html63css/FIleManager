6f3bd8e6e2df372f4862ac482e8fcdcf5dc6dffe
Sasha <html63@mail.ru>
2020-04-26 15:14:33 +0300

    проблема с help*

--- function.cpp
+++ function.cpp
@@ -274,7 +274,7 @@ void rmdir()
 void help()
 {
 	FILE* help;
-	if ((help = fopen("help.mk", "r")) != NULL)
+	if ((help = fopen("../help.mk", "r")) != NULL)
 	{
 		const int size = 2168 * 2 + 1;
 		char buf[size] = { '\0' };
@@ -286,6 +286,6 @@ void help()
 	}
 	else
 	{
-		wprintf(L"Файл не с содержанием описания функционала не открылся\n");
+		wprintf(L"Файл с функционалом открывается только при запуске программы черех .exe(из-за расположения файла с информацией) \n");
 	}
 }
